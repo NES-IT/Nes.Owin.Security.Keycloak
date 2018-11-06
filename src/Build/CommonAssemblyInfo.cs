7b91d219305ac5126f4007839ed164c19758bcab
nico.luisetto <nico.luisetto@nessrls.it>
2018-11-06 13:57:11 +0100

    change target framework to 4.6.1

--- src/Build/CommonAssemblyInfo.cs
+++ src/Build/CommonAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("NES")]
 [assembly: AssemblyCopyright("Copyright 2017 Dylan Plecki")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
